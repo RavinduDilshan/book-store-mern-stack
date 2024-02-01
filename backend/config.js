d155455de79c6199a4671b82c23690a7a1624fde
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2024-02-01 22:53:47 +0530

    book create route added

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config();
 export const PORT=5555;
 export const MONGODBURL= `mongodb+srv://root:${process.env.MONGODBPASSWORD}@books-store-mern.t1idt5u.mongodb.net/books-collection?retryWrites=true&w=majority`;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import {PORT,MONGODBURL} from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
 
 const app = express();
+
 
 app.get('/',(request,response)=>{
     console.log(request);
@@ -9,7 +13,44 @@ app.get('/',(request,response)=>{
 
 });
 
-app.listen(PORT,()=>{
-    console.log(`app is listining to port: ${PORT}`);
+//route for save a new book
+app.post('/books',async(request,response)=>{
+    try{
+        if(!request.body.title || !request.body.author || !request.body.publishYear){
+            return response.status(400).send({
+                message:'send all required fields: title,author,publishYear'
+            });
+
+        
+
+        }
+        const newBook={
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        };
+
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    }catch(error){
+        console.log(error);
+        return response.status(500).send({
+            message:error.message
+        });
+    }
+
+});
+
+
+
+mongoose.connect(MONGODBURL).then(()=>{
+    console.log('app connected to database');
+    app.listen(PORT,()=>{
+        console.log(`app is listining to port: ${PORT}`);
+    });
+}).catch((error)=>{
+    console.log(error);
+
 });
 
